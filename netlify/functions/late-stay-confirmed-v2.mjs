037d61477725c9ffe064b782aed630d86a49c09e
AstroTat808 <chris@sibel.org>
2026-09-05 06:17:17 -1000

    Remove obsolete Late Stay confirmation rewriting

--- netlify/functions/late-stay-confirmed-v2.mjs
+++ netlify/functions/late-stay-confirmed-v2.mjs
@@ -1,4 +1,3 @@
 import lateStayConfirmed from './late-stay-confirmed.mjs';
-import { rewriteLateStayResponse } from './_late-stay-response.mjs';
 
-export default async (req) => rewriteLateStayResponse(await lateStayConfirmed(req));
+export default lateStayConfirmed;
